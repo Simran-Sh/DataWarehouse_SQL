b89089eceae5f530fc87135ccc48019152f8cf7f
Simran Sharma <sssimran.sam@gmail.com>
2026-07-18 20:53:31 +0530

    init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,6 +1,7 @@
 /*
 ===================================================================
-| CREATE DATABASE AND SCHEMA's here in this init_database sql file |
+CREATE DATABASE AND SCHEMA's 
+here in this init_database sql file |
 ===================================================================
 SCRIPT PURPOSE:
 	1. Creates new database, after checking, if it doesnot already exist
@@ -8,7 +9,7 @@ SCRIPT PURPOSE:
 	3. Script will setup 3 schema's within the database  - "bronze", "silver", "gold"
 
 WARNING:
-	1. Running this script will drop entire datawarehouse and delete all data it contains 
+	1. Running this script will drop entire datawarehouse and delete all data it contains Permanently 
 
 NOTE: 
 	1. Always use "GO" to separate batches when working with multiple SQL statements
@@ -20,11 +21,12 @@ NOTE:
 Use master;
 GO
 
--- Drop and recreate database (script to run, only if working from scratch)
+-- Drop and recreate database
 IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
 		ALTER DATABASE DataWarehouse 
 		SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+		DROP DATABASE DataWarehouse;
 END;
 GO
 
@@ -35,9 +37,9 @@ GO
 USE DataWarehouse;
 GO
 
--- Create schema'as for the project
+-- Create schema'as for the project (Folder)
 CREATE SCHEMA bronze;
-GO            
+GO   -- Go is the separator in SQL         
 
 CREATE SCHEMA silver;
 GO
