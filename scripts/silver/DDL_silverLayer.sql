525bfe38d005556545588af2b7c15f3080d56f33
Simran Sharma <sssimran.sam@gmail.com>
2026-07-23 16:23:22 +0530

    Silver Layer DDL

--- scripts/silver/DDL_silverLayer.sql
+++ scripts/silver/DDL_silverLayer.sql
@@ -1,7 +1,7 @@
 /*
-============================================================
+======================
 DDL FOR SILVER LAYER
-============================================================
+======================
 Bronze layer rules:
 1. Create DDL's for silver using name convention i.e <sourcesystem>_entity>
 
@@ -30,7 +30,7 @@ CREATE TABLE silver.crm_cust_info
 	cst_firstname NVARCHAR(50),
 	cst_lastname NVARCHAR(50),
 	cst_marital_status NVARCHAR(50),
-	cst_gnder NVARCHAR(50),
+	cst_gender NVARCHAR(50),
 	cst_create_date DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
@@ -44,8 +44,9 @@ GO
 CREATE TABLE silver.crm_prd_info
 (
 	prd_id INT,
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(50),
+	cat_id NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	prd_name NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
 	prd_start_dt DATETIME,
@@ -63,9 +64,9 @@ CREATE TABLE silver.crm_sales_details
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
@@ -79,9 +80,9 @@ GO
 -- DDL for customer info table of ERP 
 CREATE TABLE silver.erp_cust_az12
 (
-	CID NVARCHAR(50),
-	BDATE DATE,
-	GEN NVARCHAR(50),
+	cid NVARCHAR(50),
+	bdate DATE,
+	gender NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -92,8 +93,8 @@ GO
 -- DDL for Loc info table of ERP
 CREATE TABLE silver.erp_loc_a101
 (
-	CID NVARCHAR(50),
-	CNTRY NVARCHAR(50),
+	cid NVARCHAR(50),
+	country NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -104,12 +105,10 @@ GO
 -- DDL for PX Category info table of ERP 
 CREATE TABLE silver.erp_px_cat_g1v2
 (
-	ID NVARCHAR(50),
-	CAT NVARCHAR(50),
-	SUBCAT NVARCHAR(50),
-	MAINTENANCE NVARCHAR(50),
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
-
-
